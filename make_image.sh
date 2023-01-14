4a5f7479e695637d442d1ff1879b13fdbe747d12
zzzzzhy <11657924@qq.com>
2023-01-14 19:22:06 +0800

    update

--- make_image.sh
+++ make_image.sh
@@ -5,12 +5,14 @@ if [[ "$DEBUG" == "1" ]]; then
   cat .config
   ls -la files
 fi
-
+cp -r custom/* packages/
+cp -r hiui/* packages/
 sed \
   -e '/^CONFIG_TARGET_ROOTFS_EXT4FS/ s/^#*/#/' \
   -i .config
   
 make image \
+  PROFILE="$PROFILE" \
   FILES="$FILES" \
   PACKAGES="$PACKAGES" \
   DISABLED_SERVICES="$DISABLED_SERVICES" \
